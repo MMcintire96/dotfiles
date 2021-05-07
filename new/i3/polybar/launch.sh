2f7b02dfb61191e8c12cd1de2cb2046f0b6fe4c4
mmcintire96 <michaeljamesmcintire@gmail.com>
2021-05-07 10:40:49 -0700

    update

--- new/i3/polybar/launch.sh
+++ new/i3/polybar/launch.sh
@@ -2,10 +2,19 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload mybar &
+    #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload mybar &
+    if [ $m != 'DP-3' ]; then
+        #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload dummyBar &
+        MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload mybar &
+        #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload centerBar &
+        #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload leftBar &
+        #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload rightBar &
+        #MONITOR=$m polybar --config=/home/michael/.config/i3/polybar/config --reload dummyBar &
+    fi
 done;
 #polybar --config=/home/michael/.config/i3/polybar/config mybar &
 
